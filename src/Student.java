80d7aed44f988ec1343ed39e4ee66492f7fc2373
yonniii <rkdtjdus753@gmail.com>
2019-05-13 14:49:07 +0900

    init method

--- src/Student.java
+++ src/Student.java
@@ -2,50 +2,50 @@
 public class Student {
 
 
-    public void StudentInfo(String aStudentNo, int Com_Score, int Eng_Score) {
-        char Com_grade;
-        char Eng_grade;
+    public void StudentInfo(String aStudentNo, int Com_Score, int Eng_Score) { //Extract Method
+        //Replace Method with Method Object
+        char temp; //Split Temporary Variable
         double avg;
 
         if(Com_Score >= 90 ) {
-            Com_grade = 'A';
+            temp = 'A';
         }
         else if (Com_Score >= 80) {
-            Com_grade = 'B';
+            temp = 'B';
         }
         else if (Com_Score >= 70 ) {
-            Com_grade = 'C';
+            temp = 'C';
         }
         else if (Com_Score >= 60 ) {
-            Com_grade = 'D';
+            temp = 'D';
         }
         else {
-            Com_grade = 'F';
+            temp = 'F';
         }
+        System.out.println("studentNo : " + aStudentNo);
+        System.out.println("Computer Score : " + Com_Score);
+        System.out.println("Computer Grade : " + temp);
 
         if(Eng_Score >= 90 ) {
-            Eng_grade = 'A';
+            temp = 'A';
         }
         else if (Eng_Score >= 80) {
-            Eng_grade = 'B';
+            temp = 'B';
         }
         else if (Eng_Score >= 70 ) {
-            Eng_grade = 'C';
+            temp = 'C';
         }
         else if (Eng_Score >= 60 ) {
-            Eng_grade = 'D';
+            temp = 'D';
         }
         else {
-            Eng_grade = 'F';
+            temp = 'F';
         }
 
         avg = (double) (Com_Score + Eng_Score)/2;
 
-        System.out.println("studentNo : " + aStudentNo);
-        System.out.println("Computer Score : " + Com_Score);
-        System.out.println("Computer Grade : " + Com_grade);
         System.out.println("English Score : " + Eng_Score);
-        System.out.println("English Grade : " + Eng_grade);
+        System.out.println("English Grade : " + temp);
         System.out.println("Average : " + avg);
 
     }
